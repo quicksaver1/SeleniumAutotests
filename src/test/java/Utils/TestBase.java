31c81b225ce72a9451a1ea37445875c7afd34184
frolov <frolov@imc-s.ru>
2023-01-24 11:06:48 +0400

    added linux support

--- src/test/java/Utils/TestBase.java
+++ src/test/java/Utils/TestBase.java
@@ -8,18 +8,30 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class TestBase {
     public ChromeDriver driver;
     public static final String MAIN_URL="http://riisz.ru";
     public static final String USERNAME ="frolov";
     public static final String PASSWORD = "1";
+    String driverPath;
 
     public static final int TIMEOUT_IN_SECONDS = 10;
     public HubPage hubPage;
 
 
     public void getDriverAndLogin () {
+        String os = System.getProperty("os.name").toLowerCase();
+
+        if(os.contains("windows")){
+            driverPath = "/driver/win/yandexdriver.exe";
+        } else if (os.contains("nux")) {
+            driverPath = "/driver/unix/yandexdriver";
+        }
+        else {
+            throw new RuntimeException("Unrecognized OS name - "+ os);
+        }
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/driver/yandexdriver.exe");
         this.driver = new ChromeDriver();
         driver.manage().window().maximize();
